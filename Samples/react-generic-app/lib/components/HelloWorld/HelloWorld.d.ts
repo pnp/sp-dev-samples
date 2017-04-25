aa3b0da35e8e1704eee0b8b09145e3b03276f48c
Patrick Rodgers <patrodg@microsoft.com>
2017-04-25 10:59:34 -0400

    Deleting folders

--- Samples/react-generic-app/lib/components/HelloWorld/HelloWorld.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import * as React from 'react';
-import IHelloWorldProps from './IHelloWorldProps';
-export declare class Hello extends React.Component<IHelloWorldProps, {}> {
-    render(): JSX.Element;
-}
--- Samples/react-generic-app/lib/components/HelloWorld/HelloWorld.module.scss.d.ts
+++
@@ -1,8 +0,0 @@
-declare const styles: {
-    helloWorld: string;
-    container: string;
-    row: string;
-    listItem: string;
-    button: string;
-};
-export default styles;
