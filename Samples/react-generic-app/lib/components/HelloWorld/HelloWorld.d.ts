2cf702d0805b80c392be3c906aef4e408670c90d
Patrick Rodgers <patrodg@microsoft.com>
2017-04-25 10:58:29 -0400

    Remove dist, lib, temp folders from git. Update to latest @microsoft modules. Move to @types, remove typings & tsd

--- Samples/react-generic-app/lib/components/HelloWorld/HelloWorld.d.ts
+++ Samples/react-generic-app/lib/components/HelloWorld/HelloWorld.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="react" />
 import * as React from 'react';
 import IHelloWorldProps from './IHelloWorldProps';
 export declare class Hello extends React.Component<IHelloWorldProps, {}> {
