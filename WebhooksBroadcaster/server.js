f7715c20c622287a0866de43f385dbb23633f05c
Giuliano De Luca <E73379@hugoboss.com>
2017-11-09 12:57:03 +0100

    added comments

--- WebhooksBroadcaster/server.js
+++ WebhooksBroadcaster/server.js
@@ -21,17 +21,19 @@ app.get('/', function (req, res) {
 // POST method route
 app.post('/', function (req, res) {
     if (req.query && req.query.validationtoken) {
+        // Validating the webhooks subscription
         console.log('Found validation token: ', req.query.validationtoken);
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end(req.query.validationtoken);
     }
     else {
         if (typeof req.body.value !== "undefined" && req.body.value !== null) {
+            // Getting the webhook and broadcasting it across Socket.IO to the client
             var data = JSON.stringify(req.body.value);
             console.log(JSON.stringify(req.body.value))
             io.emit('list:changes', data);
             const fileName = __dirname + '/public/webhooksLog.txt';
-            /* Write to file */
+            // Keeping track of every webhooks
             // Write changes in a file
             fs.exists(fileName, (exists) => {
                 let fileData = "";
